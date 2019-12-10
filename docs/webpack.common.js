757e224ff35e2501d369588d6595e72783703a6d
Joshua Minkler <josh.minkler@gmail.com>
2019-12-10 15:40:16 -0500

    Updated docs.

--- docs/webpack.common.js
+++ docs/webpack.common.js
@@ -83,7 +83,7 @@ module.exports = {
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-        template: './src/index.html'
+        template: './src/template.html'
     })
   ]
 };
