ac8034fb1e1941d57705d146f4d862a16115b19d
Joshua Minkler <josh.minkler@gmail.com>
2019-12-16 23:48:08 -0500

    Updated dependancies

--- src/components/Example1.vue
+++ src/components/Example1.vue
@@ -7,10 +7,12 @@
 
 
 <script>
-import UXRipple from '../mixins/UXRipple.js';
+import uxRipple from 'vuejs-ux-ripple';
 
 export default {
-  mixins: [UXRipple]
+  methods: {
+    uxRipple
+  }
 };
 </script>
 
